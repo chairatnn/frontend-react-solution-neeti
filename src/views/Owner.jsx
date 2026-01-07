9265d54496828dca75f0833bfa36f5d35d212ea7
chairatnn <61372139+chairatnn@users.noreply.github.com>
2026-01-07 10:20:18 +0700

    change photo

--- src/views/Owner.jsx
+++ src/views/Owner.jsx
@@ -1,19 +1,15 @@
 export default function Owner() {
   return (
     <div className="min-h-screen flex flex-col items-center pt-12 gap-y-3 w-1/2">
-      <h1 className="text-4xl font-bold text-center">02 Chairat - JSD#11</h1>
+      <h2 className="text-4xl font-bold text-center">02 Chairat - JSD#11</h2>
       <img
-        src="https://imagenes.elpais.com/resizer/v2/T5ELROQ5VFCQ5CYLJ2EMM2XVUM.jpg?auth=ade00e4f1a01974e5fd4d6eb3a2cc16ee7116e30b53264e3dfa9cbdec078c4dc&width=414"
+        src="/public/logo-genmateA.png"
         alt="user image"
         className="w-64 rounded-2xl"
       />
-      <p className="text-center">
+      <p className="text-center text-xl">
         <span className="font-bold">Short Bio:</span>
-        <br />I am José Alberto "Pepe" Mujica Cordano, I was a Uruguayan
-        politician, revolutionary and farmer who served as the 40th president of
-        Uruguay from 2010 to 2015. A former guerrilla with the Tupamaros, I was
-        tortured and imprisoned for 14 years during the military dictatorship in
-        the 1970s and 1980s.
+        <br />This generation is transforming education towards systemic employment, creating opportunities, preparing, and supporting everyone to enter meaningful careers that improve their quality of life.
       </p>
     </div>
   );
